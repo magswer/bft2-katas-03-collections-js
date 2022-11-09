a15661460c8171b5a2215c61f0786f6045d00e82
Magda <ma.wernik@gmail.com>
2022-11-09 08:32:21 +0100

    some bugs solved

--- tests.js
+++ tests.js
@@ -5,18 +5,21 @@ describe("Colecciones en JS: Array", () => {
     it("Pueden crearse usando [ ]", () => {
 
         // crea un array "a" usando los corchetes []
-
+        const a = ["item1", "item2", "item3"];    
+        
         expect(Array.isArray(a)).to.be.true;
     })
     it("Pueden crearse usando new Array", () => {
 
         // crea un array "a" usando el constructor "new"
-
+        const a = new Array("item1", "item2", "item3");
         expect(Array.isArray(a)).to.be.true;
     })
     it("Pueden estar vacíos o contener elementos", () => {
 
         // crea un array que esté vacío y otro que no esté vacío
+        const emptyArray = [];
+        const nonEmptyArray = ["item1", "item2", "item3"];
 
         expect(emptyArray).to.be.empty;
         expect(nonEmptyArray).not.to.be.empty;
@@ -27,7 +30,7 @@ describe("Colecciones en JS: Array", () => {
 
         // completa la definición que falta usando la propiedad length
 
-        expect("???").to.equal(4);
+        expect(list.length).to.equal(4);
     })
     it("Nos permiten acceder a cada elemento del array", () => {
 
@@ -35,20 +38,20 @@ describe("Colecciones en JS: Array", () => {
 
         // sustituye "???" en cada caso para acceder al elemento correspondiente de la lista
 
-        expect("???").to.equal("Piña");
-        expect("???").to.equal("Melón");
+        expect(fruits[0]).to.equal("Piña");
+        expect(fruits[3]).to.equal("Melón");
     })
     it("Nos permiten modificar cada elemento", () => {
 
         let fruits = ["Piña", "Manzana", "Fresa", "Melón"]
-
+        let manzana = "Pera";
         // Añade el código que permite modificar el elemento correspondiente
 
         expect(fruits).to.have.same.members(["Piña", "Pera", "Fresa", "Melón"])
     })
 
     it("Podemos añadir elementos", () => {
-        let fruits = ["Piña", "Manzana", "Fresa", "Melón"]
+        let fruits = ["Piña", "Manzana", "Fresa", "Melón", "Pera"]
 
         // Añade el código que permite modificar la lista (incluyendo un elemento al final)
 
@@ -148,7 +151,7 @@ describe("Operaciones iterables", () => {
         let list = [1,5,7,9,11,13];
 
         // utiliza la función map para aplicar los números de la lista
-        list = "???";
+        list = list.map;
         
 
         expect(list).to.have.same.members([0,4,6,8,10,12]);
